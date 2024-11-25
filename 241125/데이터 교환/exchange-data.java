00ab940852fed0ff822a3af3016c60de44c89eb3
parang <69078515+pjy2163@users.noreply.github.com>
2024-11-25 17:19:14 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241125/데이터 교환/exchange-data.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7;
+        int temp1;
+        int temp2;
+
+        temp1 = b;
+        temp2 = c;
+
+        b = a;
+        c = temp1;
+        a = temp2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
