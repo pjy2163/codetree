13e2d241fce4c1f514f566489c0a464797596cb3
parang <69078515+pjy2163@users.noreply.github.com>
2024-12-16 00:39:10 +0900

    Title: 4가지 관계연산자, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241216/4가지 관계연산자/four-relational-operators.java
+++ 241216/4가지 관계연산자/four-relational-operators.java
@@ -8,7 +8,7 @@ public class Main {
         int b = sc.nextInt();
 
         System.out.println(a >= b ? 1 : 0);
-        System.out.println(a >=b ? 1 : 0);
+        System.out.println(a > b ? 1 : 0);
         System.out.println(b >= a ? 1 : 0);
         System.out.println(b > a ? 1 : 0);
         }      
