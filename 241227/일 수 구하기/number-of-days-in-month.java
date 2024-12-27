bc5edd76241219e96261ecc53316ca851e5baefd
parang <69078515+pjy2163@users.noreply.github.com>
2024-12-27 22:59:07 +0900

    Title: 일 수 구하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241227/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n % 4 != 0 ) {
+            if (n == 2)
+             System.out.print(28);
+            else {
+                System.out.print(31);
+            }
+        }
+        else{
+            System.out.print(30);
+        }
+    }
+}
