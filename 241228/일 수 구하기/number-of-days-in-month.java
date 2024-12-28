68cd59e850f3c6b2129cbcd5b80bb4b4b0b8db6c
parang <69078515+pjy2163@users.noreply.github.com>
2024-12-28 23:17:29 +0900

    Title: 일 수 구하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241228/일 수 구하기/number-of-days-in-month.java
+++ 241228/일 수 구하기/number-of-days-in-month.java
@@ -9,11 +9,22 @@ public class Main {
         if (n == 2) {
             System.out.print(28);
         }
-        else if (n % 2 != 0) {
-           System.out.print(31);
+        else if(n % 2 == 0) {
+            if (n == 4 || n == 6) {
+                System.out.print(30);
+            }
+            else {
+                System.out.print(31);
+            }
         }
         else {
-            System.out.print(30);
+            if (n == 9 || n == 11) {
+                System.out.print(30);
+            }
+            else {
+                System.out.print(31);
+            }
         }
     }
 }
+       
