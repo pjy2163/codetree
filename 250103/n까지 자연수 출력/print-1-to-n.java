c0812f6c9da145421e556c968da163ee599db897
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-03 21:48:57 +0900

    Title: n까지 자연수 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250103/n까지 자연수 출력/print-1-to-n.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+
+        while (a < n) {
+            a++;
+            System.out.print(a + " ");
+        }
+    }
+}
