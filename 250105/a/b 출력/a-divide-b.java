826f806fefe647d13ff88cb79bd8f81b470ca125
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-05 17:06:46 +0900

    Title: a/b 출력, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250105/a/b 출력/a-divide-b.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i <20; i++) {
+
+            a *= 10;
+            System.out.printf("%d", a / b);
+            a %= b;
+
+        }
+    }
+}
