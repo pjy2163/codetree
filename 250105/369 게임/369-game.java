0c6afdefd6e587a5c8973e33bcd13d49d3824f00
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-05 23:46:18 +0900

    Title: 369 게임, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 250105/369 게임/369-game.java
+++ 250105/369 게임/369-game.java
@@ -7,7 +7,10 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i == 6 || i == 9) {
+            if (i % 3 == 0 || (i / 10 == 3 ||i / 10 == 6 || i / 10 == 9)) {
+                System.out.print(0 + " ");
+            }
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
                 System.out.print(0 + " ");
             }
             else {
