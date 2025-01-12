a6e97df528be0c53eed425cf6ad9315688cbed07
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-12 22:32:30 +0900

    Title: 공약수의 유무, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250112/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 250112/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -9,7 +9,7 @@ public class Main {
 
         boolean satisfied = false;
         for (int i = a; i <= b; i++) {
-            if (1920 % a == 0 && 2880 % a == 0) {
+            if (96 % a == 0) {
                 satisfied = true;
             }
         }
