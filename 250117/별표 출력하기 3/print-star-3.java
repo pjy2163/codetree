cc35353bf8f98cb614a8737362412b8184bc1565
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-17 19:21:38 +0900

    Title: 별표 출력하기 3, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 250117/별표 출력하기 3/print-star-3.java
+++ 250117/별표 출력하기 3/print-star-3.java
@@ -9,7 +9,7 @@ public class Main {
             for (int j = 0; j < i*2; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < 10-(i*2)-1; j++) {
+            for (int j = 0; j < n*2-(i*2)-1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
