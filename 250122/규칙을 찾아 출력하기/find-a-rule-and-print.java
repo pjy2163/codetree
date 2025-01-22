787188369ed7dada870090adfcebea72a7fa22b9
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-22 16:22:07 +0900

    Title: 규칙을 찾아 출력하기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 250122/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 250122/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -8,7 +8,7 @@ public class Main {
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(i == 0 || i == 5 || j == 5){
+                if(i == 0 || i == n-1 || j == n-1){
                     System.out.print("* ");
                 }
                 else if(j < i) {
