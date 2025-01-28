97da95f17ed1b89e1d0e9ed2c740ca1e06b50d9f
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-29 03:44:46 +0900

    Title: 정사각 알파벳 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250129/정사각 알파벳 출력하기/print-alpabet-in-square.java
+++ 250129/정사각 알파벳 출력하기/print-alpabet-in-square.java
@@ -7,8 +7,8 @@ public class Main {
         int n = sc.nextInt();
         //char x = sc.next().charAt(0);
         for (int i = 0; i < n; i++) {
-            for (int j = 65 + i; j < 65 + i + n; j++) {
-                System.out.print((char)(j+i));
+            for (int j = 65 + (i*n); j < 65 + (n*i)+n; j++) {
+                System.out.print((char)j);
             }
             System.out.println();
         }
