5663d9306b5573bea459f0936588558b1eebfc51
parang <69078515+pjy2163@users.noreply.github.com>
2025-01-30 14:50:49 +0900

    Title: 특정 숫자 도달하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250130/특정 숫자 도달하기/reaching-specific-number.java
+++ 250130/특정 숫자 도달하기/reaching-specific-number.java
@@ -9,11 +9,14 @@ public class Main {
         int sum = 0;
         double avg = 0;
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
             
             if (arr[i] < 250) {
                 sum += arr[i];
+                if (i == 9) {
+                    avg = (double)sum / 10;
+                }
             }
             else {
                 avg = (double)sum / i;
