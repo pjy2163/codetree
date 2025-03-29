5eca0c97e96a5b96f406d709d1bb136d8f25e110
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-03-29 22:18:37 +0900

    Title: 배열로 사각형 만들기, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250329/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] arr2d = new int[5][5];
+
+    
+        for (int j = 0; j < 5; j++) {
+            arr2d[0][j] = 1;
+            arr2d[j][0] = 1;
+        }
+
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                arr2d[i][j] = arr2d[i][j-1] + arr2d[i-1][j];
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
