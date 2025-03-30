09d0493828588ba46474bc1f5c19b35d0763885e
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-03-30 19:10:03 +0900

    Title: 지그재그로 정수 채우기, Time: 160ms, Memory: 12MB, Status: Passed - Codetree

--- 250330/지그재그로 정수 채우기/zigzag-numbering.java
+++ 250330/지그재그로 정수 채우기/zigzag-numbering.java
@@ -4,28 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = sc.nextInt(); 
+        int m = sc.nextInt(); 
 
-        int[][] arr2d = new int[n][n];
+        int[][] arr2d = new int[n][m]; 
 
         int num = 0;
 
+       
         for (int i = 0; i < m; i++) {
             if (i % 2 == 0) { 
                 for (int j = 0; j < n; j++) {
                     arr2d[j][i] = num++;
                 }
-            } 
-            else { 
-                for (int j = n - 1; j >= 0; j--) {
+            } else {
+                for (int j = n - 1; j >= 0; j--) { 
                     arr2d[j][i] = num++;
                 }
             }
         }
 
+        
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print(arr2d[i][j] + " ");
             }
             System.out.println();
