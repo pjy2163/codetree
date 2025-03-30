0dc18b6d9adc72df920f988fff14595a2a53c5f2
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-03-30 18:15:43 +0900

    Title: 격자에 점 그리기, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250330/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 1;
+
+        int[][] placed = new int[n+1][n+1];
+
+        for (int i = 1; i <= m; i++) {   
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            placed[r][c] = cnt++;
+        }
+        for (int i = 1; i <= n; i++) {   
+            for(int j = 1; j <= n; j++){
+                System.out.print(placed[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
