a5be8fdefc94374c40d08e9f2ef53e8016768603
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-03-30 19:04:32 +0900

    Title: 지그재그로 정수 채우기, Time: 128ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250330/지그재그로 정수 채우기/zigzag-numbering.java
+++ 250330/지그재그로 정수 채우기/zigzag-numbering.java
@@ -11,23 +11,24 @@ public class Main {
 
         int num = 0;
 
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i % 2 == 0) {
+        for (int i = 0; i < m; i++) {
+            if (i % 2 == 0) { 
+                for (int j = 0; j < n; j++) {
                     arr2d[j][i] = num++;
                 }
-                else {
-                    arr2d[n - 1 - j][i] = num++;
+            } 
+            else { 
+                for (int j = n - 1; j >= 0; j--) {
+                    arr2d[j][i] = num++;
                 }
             }
         }
-        for (int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(arr2d[i][j] + " ");
             }
             System.out.println();
         }
-       
     }
 }
